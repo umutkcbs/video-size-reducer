a92aadf9f777f6383a63a2e946b967b7aba373a1
Umut Görkem Kocabaş <49411067+umutkcbs@users.noreply.github.com>
2023-01-09 16:34:47 +0300

    Add files via upload

---
+++ dotnet/Pages/code.js
@@ -0,0 +1,37 @@
+﻿function dropHandler(ev) {
+    console.log('File(s) dropped');
+    document.getElementById("degisecekyazi").innerHTML = 'HARİKA! BİR DOSYA YÜKLEDİNİZ!';
+
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        [...ev.dataTransfer.items].forEach((item, i) => {
+            // If dropped items aren't files, reject them
+            if (item.kind === 'file') {
+                const file = item.getAsFile();
+                console.log(`… file[${i}].name = ${file.name}`);
+                document.getElementById("degisecekyazi").innerHTML = 'HARİKA! BİR DOSYA YÜKLEDİNİZ !';  //
+
+                //let data = document.getElementById("image-file").files[0];
+                let formData = new FormData();
+
+                formData.append("file", file);
+                fetch('/api/upload', { method: "POST", body: formData });
+            }
+        });
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        [...ev.dataTransfer.files].forEach((file, i) => {
+            console.log(`… file[${i}].name = ${file.name}`);
+            document.getElementById("degisecekyazi").innerHTML = 'HARİKA! SADECE BİR DOSYA YÜKLEDİNİZ!';
+        });
+    }
+}
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
---
+++ dotnet/Processor/VideoProcessor.cs
@@ -0,0 +1,40 @@
+﻿using FFMpegCore;
+using FFMpegCore.Enums;
+using Microsoft.AspNetCore.Components.Forms;
+using OpenCvSharp;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using VideoEditorBE.Controllers;
+using static System.Net.Mime.MediaTypeNames;
+
+//[DllImport("user32.dll")]
+
+namespace VideoEditorBE.Processor
+{
+    public class VideoProcessor
+    {
+        public static void Processs(string fPath)
+        {
+            EditorController.status = "Processing...";
+            try
+            {
+                string outputName = DateTime.Now.Ticks.ToString() + ".mp4";
+
+                Process process = new Process();
+                process.StartInfo.FileName = "ffmpeg.exe";
+                process.StartInfo.Arguments = "-i " + fPath + " -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" -acodec copy -threads 12 -vcodec libx264 -crf 28 " + outputName;
+                process.Start();
+
+                Console.WriteLine("11111111111111111111111111111111111111111111");
+                
+            }
+            catch (Exception ex)
+            {
+                //return ex.ToString();
+                EditorController.status = ex.ToString();
+            }
+
+        }
+    }
+}
